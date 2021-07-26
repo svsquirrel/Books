3ff2f34d18d4b883f09a316f14985de0e643308f
svsquirrel <suekittle@hotmail.com>
2021-07-26 17:22:23 -0500

    Second commit. Got some of it working but now need to code how to display the library

--- books.js
+++ books.js
@@ -1,155 +1,78 @@
 
 let myLibrary = [];
-const inputTitle = document.querySelector('#title');
-const inputAuthor = document.getElementById('author');
-const inputGenre = document.getElementById('genre');
-const inputRead = document.getElementById('read');
-const display   =  document.getElementById('displaycrap')
+let book;
 const form = document.getElementById ('popupForm');
-const enters = document.getElementById('enter');
-    enters.addEventListener('click', () => {
-        addBooks()
-     });
-
-document.addEventListener('DOMContentLoaded', function() {
-    var pageLibrary = JSON.parse(localStorage.getItem('myLibrary'));
-         if (pageLibrary == null) {
-             alert ("add some books!");
-         };
-    alert(pageLibrary.length);
-    }, false);
-
- var Books = function (title, author, genre, read){
+const suggestedLibrary = [
+    {title: 'Murder on the Orient Express', author: 'Agatha Christie', genre: 'Mystery', read: 'Yes'},
+    {title: 'Learning Javascript', author: 'Ethan Brown', genre: 'non-fiction', read: 'No'}
+];
+ class Books {
+    constructor(title, author, genre, read){
         this.title = title,
         this.author = author,
         this.genre = genre,
         this.read = read
-};
+    }
+} 
 
-//function getBooks() {
-    //get the library array from localstorage
-//    
-//}
-
-function addBooks(){
-   // get the new book, add to myLibrary and send to localstorage
-  
-   /* var title = inputTitle.value;
-    var author = inputAuthor.value;
-    var genre = inputGenre.value;
-    var read = inputRead.value;
-    var entry = {
-        'title': title,
-        'author': author,
-        'genre': genre,
-        'read': read,
-       };
-   // localStorage.setItem('entry', JSON.stringify(entry));
-  *///  existingLibrary.push(entry);
+function setLibrary() {
     localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
- };
-
- function openForm() {
-    form.style.display = 'block';
 }
 
-function closeForm() {
-   form.style.display = 'none';
+function getLibrary() {
+   if(!localStorage.myLibrary){
+       return
+   }else {
+   let tempLibrary = localStorage.getItem('myLibrary')
+   tempLibrary = JSON.parse(tempLibrary);
+   myLibrary = tempLibrary;
+   }
 }
 
+const enters = document.getElementById('enter');
+      enters.addEventListener('click' , () => {
+                 getBookFromForm()
+})  
+
+function getBookFromForm() {
+    
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let genre = document.getElementById('genre').value;
+    let read = document.getElementById('read').checked;
+    let book = new Books(title, author, genre, read);
+    myLibrary.push(book);
+    setLibrary();
+   
+ };  
  
+document.addEventListener('DOMContentLoaded', () => {
+     getFixedLibrary(), getLibrary()
+});  
+ 
+ function getFixedLibrary (){
+    var sugFirst = document.getElementById('sugfirst');
+    var sugSecond = document.getElementById('sugsecond')
+   // sugFirst.textContent  = suggestedLibrary[0];
+    bookOne = Object.values(suggestedLibrary[0]);
+    bookTwo = Object.values(suggestedLibrary[1]);
+    sugFirst.innerHTML = bookOne.join('<br>');
+    sugSecond.innerHTML = bookTwo.join('<br>');
+}
 
+function displayLibrary() {
+   // var displayBooks = JSON.parse(localStorage.getItem('myLibrary'))
 
-/*
- var theHobbit = new Books('the Hobbit', 'J.R.R. Tolkein', 'Fantasy', 'no');
-var emma = new Books('Emma', 'JaneEyre', 'Fiction', 'no'); 
-myLibrary.push(emma, theHobbit);
-function addEntry() {
-    // Parse any JSON previously stored in allEntries
-    var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
-    if(existingEntries == null) existingEntries = [];
-    var entryTitle = document.getElementById("entryTitle").value;
-    var entryText = document.getElementById("entryText").value;
-    var entry = {
-        "title": entryTitle,
-        "text": entryText
-    };
-    localStorage.setItem("entry", JSON.stringify(entry));
-    // Save allEntries back to local storage
-    existingEntries.push(entry);
-    localStorage.setItem("allEntries", JSON.stringify(existingEntries));
-};
-
-
-  
-  // const author= inputAuthor.target.value;
-  // const genre = inputGenre.target.value;
-  // const read = inputRead.target.value;
-
-  
+}
 function openForm() {
     form.style.display = 'block';
-
 }
 
 function closeForm() {
    form.style.display = 'none';
 }
+ 
+ 
+ 
 
-function editRead() {
-
-}
-
-function getBookImage() {
-
-}
-
-
-   
-
-
-   //use capital letter if it's a function constructor
-
-make the form:
-https://www.w3docs.com/tools/code-editor/12095
-
-local storage:
-https://www.youtube.com/watch?v=k8yJCeuP6I8
-
-var bookProto = {
-    output: function (){
-        sentence = this.title +' by ' +this.author +', '+this.pages + ' pages '+', '+ this.read;
-        return sentence 
-    }
-}
-
-var theHobbit = Object.create(bookProto, 
-   {
-       title : { value: 'The Hobbit' },
-       author: { value: 'J.R.R. Tolkien'},
-       pages:  { value: '295'},
-       read:  {  value: 'not read yet'}
-   } );
-
-
-
-var Books = function (title, author, genre, read){
-    this.title = title,
-    this.author = author,
-    this.genre = genre,
-    this.read = read
-};
-
-Books.prototype.output = 
-    function() {
-        sentence = this.title +' by ' +this.author +', '+this.pages + ' pages '+', '+ this.read;
-        return sentence
-    };
-
-var theHobbit = new Books('the Hobbit', 'J.R.R. Tolkein', '295', 'not read yet');
-      
-console.log(theHobbit.output());
-*/
-
-//object.create  Just another way of books.prototype.output but making the prototype the output function
-//not the Books constructor. Will get same data on console.log
+ https://stackoverflow.com/questions/63310980/how-to-display-my-array-of-objects-into-my-html-pages-table-individually-by
